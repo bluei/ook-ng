ab494bb383b863a3df4b72554aef434d9eef190e
Scott Morey <github@bluei.net>
2018-08-03 23:06:07 -0400

    Initial Commit

---
+++ OOK.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using OOK.API.Models;
+
+namespace OOK.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values {get; set;}
+    }
+}
